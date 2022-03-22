081833ac20e5179aa3e2c30a99fa7735820308c9
Семён Вервейн <semen.vervein@gmail.com>
2022-03-22 20:30:45 +0700

    Chord creation test

---
+++ ChorderTests/ChordTests.cs
@@ -0,0 +1,15 @@
+﻿using Chorder;
+using NUnit.Framework;
+
+namespace ChorderTests
+{
+    class ChordTests
+    {
+        [Test]
+        public void ScannerClassCreationTest()
+        {
+            Chord chord = new Chord();
+            Assert.NotNull(chord);
+        }
+    }
+}
