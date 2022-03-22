acb101aeab915b4563a5dee3194dae2401eb4977
Семён Вервейн <semen.vervein@gmail.com>
2022-03-22 20:03:18 +0700

    Пофиксил проект для тестов и добавил тест "Scanner creation test"

---
+++ ChorderTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace ChorderTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
